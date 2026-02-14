aa2e36c0cce943886ed1cde29fd4f7b4b925cff1
Andriani <info@acisindonesia.com>
2026-02-14 13:26:57 +0700

    update dashboard3

--- app.py
+++ app.py
@@ -50,11 +50,6 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "error"
 
-with app.app_context():
-    db.create_all()
-    ensure_schema()
-    ensure_seed_data()
-
 # -----------------------
 # Login Manager
 # -----------------------
@@ -1153,3 +1148,11 @@ def ensure_schema():
 
         # --- Table attachment ---
         # kalau belum ada, biarkan create_all handle. (Postgres: create_all akan bikin)
+
+# -----------------------
+# Init DB (create tables + schema + seed)
+# -----------------------
+with app.app_context():
+    db.create_all()
+    ensure_schema()
+    ensure_seed_data()
